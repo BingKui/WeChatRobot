e3b9d3aa9864d4838b9b16551243919f1a987e54
康兵奎 <178974662@qq.com>
2018-06-29 16:34:35 +0800

    update

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-const qs = require('querystring');
-const axios = require('axios');
-const fs = require('fs');
-var dayjs = require('dayjs');
-const {
-    baiduApiKey,
-    baiduSecretKey
-} = require('./config.js');
+// const qs = require('querystring');
+// const axios = require('axios');
+// const fs = require('fs');
+// const {
+//     baiduApiKey,
+//     baiduSecretKey
+// } = require('./config.js');
 
-const url = 'https://aip.baidubce.com/oauth/2.0/token';
-const param = qs.stringify({
-    grant_type: 'client_credentials',
-    client_id: baiduApiKey,
-    client_secret: baiduSecretKey
-});
+// const url = 'https://aip.baidubce.com/oauth/2.0/token';
+// const param = qs.stringify({
+//     grant_type: 'client_credentials',
+//     client_id: baiduApiKey,
+//     client_secret: baiduSecretKey
+// });
 // const { accessToken } = require('./token/accessToken');
 // console.log('改变之前的Accesstoken为：', accessToken);
 // axios.post(`${url}?${param}`).then((response) => {
@@ -38,7 +37,22 @@ const param = qs.stringify({
 
 // console.log(dayjs());
 
-const now = dayjs();
-console.log(now.valueOf())
-const indate = (now.add(30, 'day')).valueOf();
-console.log(indate - now.valueOf())
+// const now = dayjs();
+// console.log(now.valueOf())
+// const indate = (now.add(30, 'day')).valueOf();
+// console.log(indate - now.valueOf())
+
+const schedule = require('node-schedule');
+const dayjs = require('dayjs');
+// 定时任务测试
+const testSchedule = () => {
+    schedule.scheduleJob('* * * * * *', function() {
+        const now = dayjs().format('YYYY-MM-DD HH:mm:ss');
+        console.log(now);
+    });
+    schedule.scheduleJob('* * * * * *', function() {
+        console.log('dishiqi ');
+    });
+}
+
+testSchedule();
